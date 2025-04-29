8820381985cd4fa008bca0d4275db0163436870a
AlltechMVP <daltondeleon2011@gmail.com>
2025-04-29 02:52:26 +0000

    Add base setting to fix Vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // <-- ADD THIS LINE at top level
   server: {
     host: '0.0.0.0',
     port: 5000,
